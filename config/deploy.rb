0e03000c05b8eaf67c45d9a68a1e7b53ae488dab
Mathias Peltier <peltiermathias@gmail.com>
2011-09-15 09:58:56 +0200

    Add rvm config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,17 @@
 require 'bundler/capistrano'
 
+# To use rvm config
+set :default_environment, { 
+  'PATH' =>"/usr/local/rvm/bin:/usr/local/rvm/gems/ruby-1.9.2-p290/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/bin:/usr/kerberos/bin:/bin:/usr/bin:/home/zad/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2p290',
+  'GEM_HOME' => '/usr/local/rvm/gems/ruby-1.9.2-p290',
+  'GEM_PATH' => '/usr/local/rvm/gems/ruby-1.9.2-p290:/usr/local/rvm/gems/ruby-1.9.2-p290@global' 
+}
+
 set :user, 'zad'
 set :domain, 'ulla.bugz.fr'
 set :application, 'zadkiel.me'
 
-# the rest should be good
 set :repository,  "git@github.com:zadkiel87/zadkiel-me.git" 
 set :deploy_to, "/home/#{user}/public_html/www/#{application}"
 set :deploy_via, :remote_cache
